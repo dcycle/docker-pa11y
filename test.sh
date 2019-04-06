77c8ee1e8c8ca9accd90384cd877f88168c704bc
alberto56 <albert@mediatribe.net>
2019-04-06 09:39:48 -0400

    Please squash this commit in the pull request

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ docker pull dcycle/browsertesting:3
 docker pull dcycle/pa11y
 docker build -t local-dcycle-pa11y-image .
 
-docker run dcycle/pa11y
-docker run local-dcycle-pa11y-image
+docker run --rm dcycle/pa11y:1
+docker run --rm local-dcycle-pa11y-image
