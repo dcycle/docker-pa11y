8a029ee47b08ee23564efe6e461a3759fe4cc345
Albert Albala <albert@mediatribe.net>
2021-10-26 16:43:24 -0400

    https://github.com/dcycle/docker-digitalocean-php/commit/53a566dcbb6dfde9e8c9b3fe3dfe88c298cafda4

--- scripts/jenkins/rebuild.sh
+++ scripts/jenkins/rebuild.sh
@@ -4,12 +4,19 @@ source ~/.docker-host-ssh-credentials
 
 # Create a droplet
 DROPLET_NAME=docker-pa11y
-PRIVATE_IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
+IP1=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" \
   "./digitalocean/scripts/new-droplet.sh $DROPLET_NAME")
 # https://github.com/dcycle/docker-digitalocean-php#public-vs-private-ip-addresses
-IP=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$PRIVATE_IP" --after-context=10|tail -1|cut -b 44-)
+IP2=$(ssh "$DOCKERHOSTUSER"@"$DOCKERHOST" "./digitalocean/scripts/list-droplets.sh" |grep "$IP1" --after-context=10|tail -1|cut -b 44-)
+echo "Now determining which of the IPs $IP1 or $IP2 is the public IP"
+if [[ $IP1 == 10.* ]]; then
+  IP="$IP2";
+else
+  IP="$IP1";
+fi
 echo "Created Droplet at $IP"
 sleep 90
+
 ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
   root@"$IP" "mkdir -p docker-pa11y-job"
 scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
